4368b6587a4c431a2997d7d7c2af98ddbf62129c
Selma Delgado <selmadelgado@gmail.com>
2018-09-07 19:57:41 -0500

    Bug fix

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 router.get('/', function(req,res){
-   burger.all(function(burger_data){
+   burger.all(function('index'){
       console.log(burger_data);
         res.render('index', {burger_data});
     })
