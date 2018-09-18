16abfccbbe378bbc7db7fb192f011a7de2a564d6
Selma Delgado <selmadelgado@gmail.com>
2018-09-18 12:43:31 -0500

    Fixed connection to mySQL

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
 });
 
-connection.connect(function(err){
-  if (err) throw err;
-  console.log("Connected as id:" + connection.threadid);
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
